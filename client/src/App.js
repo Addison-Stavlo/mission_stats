e8830b69c352e1df16a96eeeb4437e163c538e64
Addison-Stavlo <AddieStavlo@gmail.com>
2019-08-05 22:18:01 -0400

    apollo added and provider set

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import bootstrapCyborgTheme from "./bootstrap.cyborg.css";
-
+import logo from "./spaceX_logo.PNG";
 import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>SpaceX</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <img
+          src={logo}
+          alt="SpaceX"
+          style={{ width: 300, display: "block", margin: "0 auto" }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
